cea9a84c905aa3c1c86dc79764c69da1f75c4056
Jayakmar <jaeindia@gmail.com>
2015-08-26 23:32:41 +0800

    ownCloud Enhancements - Entire project commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# ownCloud
+ownCloud gives you freedom and control over your own data.
+A personal cloud which runs on your own server.
+
+# Implemented features
+ - Virtual/Interactive Tour
+ - Session Management
+ - Invitation to comment
+ - Shared comments
+ - Show online users viewing the same doc
---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+$CONFIG = array(
+"passwordsalt" => '76702743376037392765769621433074',
+"datadirectory" => '/home/student/public_html/owncloud/data',
+"dbtype" => 'mysql',
+"version" => '4.0.2',
+"dbname" => 'owncloud',
+"dbhost" => 'localhost',
+"dbtableprefix" => 'oc_',
+"dbuser" => 'oc_student',
+"dbpassword" => '5a1d1e840c2da58ac2d5582164cee1b5',
+"installed" => true,
+);
+?>
